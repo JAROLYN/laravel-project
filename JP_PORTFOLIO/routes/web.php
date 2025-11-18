a7cffefcf2e837b75f23c5a99f563c63fec8f30a
JAROLYN <jpayumo23-5021@cca.edu.ph>
2025-11-18 17:06:32 +0800

    jarolyn
    jaja

---
+++ JP_PORTFOLIO/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function(){
+    return view('welcome');
+})
+
+Route::get('/register' [AuthController::class, 'showRegister'])->name('register.form');
+Route::post('/register' [AuthController::class, 'performRegister'])->name('register');
+
+Route::get('/login' [AuthController::class, 'showLogin'])->name('register.form');
+Route::post('/login' [AuthController::class, 'performLogin'])->name('login');
+
+
+Route::get('/logout', function(){
+
+})
