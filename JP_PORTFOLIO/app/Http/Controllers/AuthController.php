dc698a3bc22814e0b57cd965f9293c67c682747f
JAROLYN <jpayumo23-5021@cca.edu.ph>
2025-12-09 23:29:40 +0800

    Update AuthController.php

--- JP_PORTFOLIO/app/Http/Controllers/AuthController.php
+++ JP_PORTFOLIO/app/Http/Controllers/AuthController.php
@@ -16,22 +16,24 @@ class AuthController extends Controller
     function showLogin(){
         return view('authentication.login');
     }
-    function register(Request $request){
 
+    function register(Request $request){
         $request->validate([
             'name' => 'required|string|max:255',
-            'email'     => 'required|email|unique:users',
-            'password'  => 'required|min:6|confirmed',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
         ]);
 
         User::create([
             'full_name' => $request->full_name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('login.form')->with('success','Registration successful!')
+        return redirect()->route('login.form')->with('success', 'Registration successful!');
     }
+
     function login(Request $request){
+        // You still need to add login logic here!
     }
-} gdhdh
+}
